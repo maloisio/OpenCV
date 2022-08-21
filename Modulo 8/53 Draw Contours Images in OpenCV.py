70708bceba940ec376d9043579ddd3383465c899
Mauro Smaniotto <maloisio_otto@hotmail.com>
2022-08-21 19:58:20 -0300

    mod pc

--- Modulo 8/53 Draw Contours Images in OpenCV.py
+++ Modulo 8/53 Draw Contours Images in OpenCV.py
@@ -3,14 +3,17 @@ import numpy as np
 
 
 # cv.line(img, (int(imgWidth/2),0), (int(imgWidth/2), imgHeight), (0,0,255), 3)
-img = cv.imread("..\pneu2.jpg")
+imgInit = cv.imread("..\pneu2.jpg")
+img = cv.GaussianBlur(imgInit, (5, 5), 0)
+
+hull  =  []
 dimensions = img.shape
 imgHeight = img.shape[0]
 imgWidth = img.shape[1]
 imgChannels = img.shape[2]
 
-#img2 = np.zeros([imgWidth, imgHeight, imgChannels], np.uint8)  # criacao imagem preta
-bbox = cv.selectROI("Tracking", img, False)
+
+#bbox = cv.selectROI("Tracking", img, False)
 
 def display():
     pass
@@ -24,20 +27,23 @@ cv.createTrackbar("H", "Gray Tracker", 0, 255, display)
 cv.createTrackbar("A", "Gray Tracker", 0, 15000, display)
 #cv.createTrackbar("Ab", "Gray Tracker", 15000, 0, display)
 
-drawBox(img, bbox)
+#drawBox(img, bbox)
 
-img2 = np.zeros([int(bbox[2])-int(bbox[0]), int(bbox[3])-int(bbox[1]), imgChannels], np.uint8)
-newFrame = img[int(bbox[1]):int(bbox[1]) + int(bbox[3]),int(bbox[0]):int(bbox[0]) + int(bbox[2])]  # pegando a bola da imagem original
+#frame2 = np.zeros([700, int(bbox[3])-int(bbox[1]), imgChannels], np.uint8)
 
-img2[int(bbox[1]):int(bbox[1]) + int(bbox[3]), int(bbox[0]):int(bbox[0]) + int(bbox[2])] = newFrame
 
 while True:
+    img2 = np.zeros([imgWidth, imgHeight, imgChannels], np.uint8)  # criacao imagem preta
+   ## newFrame = img[int(bbox[1]):int(bbox[1]) + int(bbox[3]),
+   ## int(bbox[0]):int(bbox[0]) + int(bbox[2])]  # pegando a bola da imagem original
+
+    ##img2[int(bbox[1]):int(bbox[1]) + int(bbox[3]), int(bbox[0]):int(bbox[0]) + int(bbox[2])] = newFrame
     img = cv.imread("..\pneu2.jpg")
     imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img2Gray = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
     lH = cv.getTrackbarPos("H", "Gray Tracker")
     lA = cv.getTrackbarPos("A", "Gray Tracker")
-    lAb = cv.getTrackbarPos("Ab", "Gray Tracker")
+    #lAb = cv.getTrackbarPos("Ab", "Gray Tracker")
 
 
 
@@ -52,24 +58,41 @@ while True:
     contours2, hierachy = cv.findContours(thresh3, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     print("Countour Numbers = " + str(len(contours)))
 
+    for i in range(len(contours)):
+        hull.append(cv.convexHull(contours[i]))
+
+
+    # print(contours[0])
+    #if len(contours) != 0:
+     #   for contours in contours:
+      #      if (cv.contourArea(contours) > lA):  # para nao ficar pegando pontos pequenos
+       #         x, y, w, h = cv.boundingRect(contours)
+        #        cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 3)
+         #       cv.drawContours(img, contours, -1, (0, 255, 0), 2)
+          #      cv.drawContours(img2, contours, -1, (0, 255, 255), 2)
+
     # print(contours[0])
     if len(contours) != 0:
         for contours in contours:
             if (cv.contourArea(contours) > lA):  # para nao ficar pegando pontos pequenos
-                x, y, w, h = cv.boundingRect(contours)
-                cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 3)
-                cv.drawContours(img, contours, -1, (0, 255, 0), 2)
+                rect= cv.minAreaRect(contours)
+                box= cv.boxPoints(rect)
+                box= np.int0(box)
+                cv.drawContours(img, [box], -1, (0, 255, 0), 2)
                 cv.drawContours(img2, contours, -1, (0, 255, 255), 2)
+                cv.drawContours(img2, [box], -1, (0, 255, 255), 2)
+
+                    cv.drawContours(img, hull, i, [0, 0, 255], 1, 8)
 
 
 
     cv.imshow("Image23", img2)
-    cv.imshow("Threash3", thresh3)
-    cv.imshow("Gray Image2", img2Gray)
+    #cv.imshow("Threash3", thresh3)
+    #cv.imshow("Gray Image2", img2Gray)
 
-    cv.imshow("Image2", thresh2)
+    #cv.imshow("Image2", thresh2)
     cv.imshow("Image", img)
-    cv.imshow("Gray Image", imgGray)
+   # cv.imshow("Gray Image", imgGray)
 
     key = cv.waitKey(1)
     if key == 27:
